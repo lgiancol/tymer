a14e4fb38d15632e79aad2f2218b637003ff5de2
Lucas Giancola <lgiancol@github.com>
2022-09-08 23:22:39 -0400

    OMG better be the final removed log

--- src/app/pipes/respect-new-lines.pipe.ts
+++ src/app/pipes/respect-new-lines.pipe.ts
@@ -6,10 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RespectNewLinesPipe implements PipeTransform {
 
     transform(str: string): string {
-        const r = str.replace("\n", "<br>");
-        console.log(r);
-
-        return r
+        return str.replace("\n", "<br>");
     }
 
 }
