17b63f78d8088901db9c773c6867d6f30dbe12dd
Lucas Giancola <lgiancol@github.com>
2022-09-26 21:20:37 -0400

    Ordered the timesheets by startDate descending

--- src/app/timesheets/timesheets/timesheets.component.ts
+++ src/app/timesheets/timesheets/timesheets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map, Subscription } from 'rxjs';
 import { TimeSheet } from 'src/app/model/timesheet';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'tymer-timesheets',
@@ -20,9 +21,13 @@ export class TimesheetsComponent implements OnInit, OnDestroy {
     ngOnInit(): void {
         this.subs.add(TimeSheet.Collection(this.firebase).valueChanges({ idField: 'id' }).pipe(map(ts => ts.map(ts => TimeSheet.fromFirebase(ts)))).subscribe(timeSheets => {
             if (timeSheets != null) {
-                this.timeSheets = timeSheets as TimeSheet[];;
+                this._initTimeSheets(timeSheets as TimeSheet[]);
             }
         }));
+    }
+
+    private _initTimeSheets(timeSheets: TimeSheet[]) {
+        this.timeSheets = _.orderBy(timeSheets, 'startDate', 'desc');
     }
 
     ngOnDestroy(): void {
